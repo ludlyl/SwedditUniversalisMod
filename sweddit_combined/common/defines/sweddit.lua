b5c2f3622753e4b7f2d88e9203dd309425f4a920
ludlyl <ludvig.lyl@gmail.com>
2020-02-28 16:14:37 +0100

    Rename bankruptcy removes building from 8 years (default is 5) Voted in with 18 votes to 14 (56.3% support) For season 21

--- sweddit_combined/common/defines/sweddit.lua
+++ sweddit_combined/common/defines/sweddit.lua
@@ -1,7 +1,6 @@
 -- Format for overwriting define values:
 --
 
-NDefines.NEconomy.BANKRUPTCY_BUILDING_DESTRUCTION_THRESHOLD = 8
 NDefines.NCountry.COUNTRIES_GETTING_SCORE = 0
 NDefines.NMilitary.WARGOAL_MAX_BONUS = 35
 
