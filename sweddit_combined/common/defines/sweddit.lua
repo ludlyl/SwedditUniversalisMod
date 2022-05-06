d33bdf23bf2324650e6fe3fb5d401155e6c56698
ludlyl <ludvig.lyl@gmail.com>
2022-05-07 01:12:07 +0200

    Remove typo comma in defines.lua

--- sweddit_combined/common/defines/sweddit.lua
+++ sweddit_combined/common/defines/sweddit.lua
@@ -3,7 +3,7 @@ NDefines.NMilitary.WARGOAL_MAX_BONUS = 35
 NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
 
 NDefines.NMilitary.SLACKEN_AP_DROP = 0.25
-NDefines.NCountry.PS_ASSAULT = 50,
+NDefines.NCountry.PS_ASSAULT = 50
 
 NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 0
 NDefines.NCountry.PS_REPLACE_RIVAL = 0
